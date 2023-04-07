f61829b84d8fe1ae184f779a0998e39e4cac0fc3
Anton Roters <antonroters@hotmail.com>
2023-04-07 11:19:00 +0200

    refactor event sorting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,26 +1,23 @@
 import Layout from "../components/Layout";
 import GlobalStyle from "../styles";
 import uniqueCities from "../utils/getCities";
-import sortedEvents from "../utils/sortEvents";
+import { getSortedEvents, getEventsOfCity } from "../utils/getEvents";
+import { events } from "../lib/data";
 import { useFilterStore } from "../store";
 
 export default function App({ Component, pageProps }) {
   const city = useFilterStore((state) => state.city);
-
-  const events = city
-    ? sortedEvents.filter((event) => {
-        const addressArray = event.location.address.split(",");
-        const postalCity = addressArray[addressArray.length - 2];
-        const cityString = postalCity?.replace(/\d+/, "").trim();
-        return cityString === city;
-      })
-    : sortedEvents;
+  const sortedEventsOfCity = getEventsOfCity(city, getSortedEvents(events));
 
   return (
     <>
       <GlobalStyle />
       <Layout>
-        <Component {...pageProps} events={events} cities={uniqueCities} />
+        <Component
+          {...pageProps}
+          events={sortedEventsOfCity}
+          cities={uniqueCities}
+        />
       </Layout>
     </>
   );
