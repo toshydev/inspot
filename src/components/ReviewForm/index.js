a968d307a3405a9428b404150c61d1b4eb50d9a2
Anton Roters <antonroters@hotmail.com>
2023-04-18 17:40:01 +0200

    improve input elements

--- src/components/ReviewForm/index.js
+++ src/components/ReviewForm/index.js
@@ -35,37 +35,16 @@ export default function ReviewForm({ id }) {
         <label htmlFor="user">Name: </label>
         <input id="user" name="user"></input>
         <fieldset>
-          <label id="rating">Rating: </label>
-          <input
-            type="radio"
-            value={2}
-            aria-labelledby="rating"
-            name="rating"
-          />
-          <input
-            type="radio"
-            value={3}
-            aria-labelledby="rating"
-            name="rating"
-          />
-          <input
-            type="radio"
-            value={4}
-            aria-labelledby="rating"
-            name="rating"
-          />
-          <input
-            type="radio"
-            value={5}
-            aria-labelledby="rating"
-            name="rating"
-          />
-          <input
-            type="radio"
-            value={1}
-            aria-labelledby="rating"
-            name="rating"
-          />
+          <input type="radio" id="one" value={1} name="rating" />
+          <label for="one">1</label>
+          <input id="two" type="radio" value={2} name="rating" />
+          <label for="two">2</label>
+          <input id="three" type="radio" value={3} name="rating" />
+          <label for="three">3</label>
+          <input id="four" type="radio" value={4} name="rating" />
+          <label for="four">4</label>
+          <input id="five" type="radio" value={5} name="rating" />
+          <label for="five">5</label>
         </fieldset>
         <label htmlFor="attended">Attended? </label>
         <input type="checkbox" id="attended" name="attended" />
