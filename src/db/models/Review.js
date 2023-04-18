674fc2644384ed626df2b77820a3a71b71ac3f45
Anton Roters <antonroters@hotmail.com>
2023-04-18 12:43:30 +0200

    create db models

---
+++ src/db/models/Review.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+  parent: { type: String, required: true },
+  user: { type: String, required: true },
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  rating: { type: Float32Array, required: true },
+  attended: { type: Boolean, required: true },
+  timestamp: { type: Date, required: true },
+});
+
+const Review = mongoose.models.Review || mongoose.model("Review", reviewSchema);
+
+export default Review;
